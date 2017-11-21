8844f225c276177e803d48d8502240235403c5f3
bgulbis <brian.gulbis@gmail.com>
2017-11-21 15:45:22 -0600

    tidy data

--- src/01_get-data.R
+++ src/01_get-data.R
@@ -25,9 +25,45 @@ id <- read_data(dir_raw, "id", FALSE) %>%
 
 mbo_id <- concat_encounters(id$millennium.id)
 
+data_patients <- pts %>%
+    left_join(id, by = "fin") %>%
+    arrange(millennium.id, code_datetime) %>%
+    distinct(millennium.id, .keep_all = TRUE)
+
 # run MBO queries
-#   * Clinical Events - Prompt
-#       - Clinical Event: SpO2 percent;Apical Heart Rate;Respiratory Rate
+#   * Clinical Events - No Order Id - Prompt
+#       - Clinical Event: SpO2 percent;Apical Heart Rate;Respiratory Rate;Peripheral Pulse Rate
 #   * Labs - CBC
 #   * Vitals - BP
 #   * Vitals - Temp
+
+labs <- read_data(dir_raw, "labs", FALSE) %>%
+    as.labs() %>%
+    tidy_data() %>%
+    inner_join(data_patients, by = "millennium.id") %>%
+    filter(lab %in% c("hgb", "hct"),
+           lab.datetime >= code_datetime - hours(24),
+           lab.datetime <= code_datetime + hours(24))
+
+vitals_bp <- read_data(dir_raw, "vitals-bp", FALSE) %>%
+    as.vitals() %>%
+    inner_join(data_patients, by = "millennium.id") %>%
+    filter(vital.datetime >= code_datetime - hours(24),
+           vital.datetime <= code_datetime + hours(24)) %>%
+    select(millennium.id, code_datetime, vital.datetime, vital, vital.result)
+
+vitals_hr <- read_data(dir_raw, "vitals-hr", FALSE) %>%
+    as.events(order_var = FALSE) %>%
+    inner_join(data_patients, by = "millennium.id") %>%
+    filter(event.datetime >= code_datetime - hours(24),
+           event.datetime <= code_datetime + hours(24)) %>%
+    select(millennium.id, code_datetime, vital.datetime = event.datetime, vital = event, vital.result = event.result) %>%
+    mutate_at("vital.result", as.numeric)
+
+data_vitals <- vitals_bp %>%
+    bind_rows(vitals_hr) %>%
+    mutate_at("vital.datetime", funs(floor_date(., unit = "min"))) %>%
+    arrange(millennium.id, vital.datetime) %>%
+    distinct(millennium.id, code_datetime, vital.datetime, vital, .keep_all = TRUE) %>%
+    group_by(millennium.id, code_datetime) %>%
+    spread(vital, vital.result)
